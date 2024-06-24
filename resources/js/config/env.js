9a2e0badf4ef57f195bae32f35edb3d9880fe54b
mike <omodingmike@gmail.com>
2024-06-24 22:31:06 +0300

    Remove .env from repository and add to .gitignore

--- resources/js/config/env.js
+++
@@ -1,9 +0,0 @@
-const ENV = {
-    // API_URL: process.env.MIX_HOST,
-    // API_URL:'https://smartduuka-restaurant.test',
-    API_URL:'https://moodbox.smartduuka.com',
-    API_KEY: process.env.MIX_API_KEY,
-    GOOGLE_MAP_KEY: process.env.MIX_GOOGLE_MAP_KEY,
-    DEMO: process.env.MIX_DEMO
-};
-export default ENV;
