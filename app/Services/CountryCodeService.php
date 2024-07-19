608d2536403d6f30785c30e0d20a9ff691afac03
barakhubs <markbrightbaraka22@gmail.com>
2024-07-19 12:31:04 +0300

    fixed country code issue

--- app/Services/CountryCodeService.php
+++ app/Services/CountryCodeService.php
@@ -2,13 +2,17 @@
 
 namespace App\Services;
 
-
 use Exception;
 use Illuminate\Support\Facades\Log;
-use PragmaRX\Countries\Package\Countries;
 
 class CountryCodeService
 {
+    private $countries;
+
+    public function __construct()
+    {
+        $this->countries = $this->getCountriesList();
+    }
 
     /**
      * @throws Exception
@@ -16,15 +20,7 @@ class CountryCodeService
     public function list(): array
     {
         try {
-            $countryArray = [];
-            $countries    = Countries::all();
-            foreach ($countries as $key => $country) {
-                $countryArray[] = (object)[
-                    'country_code' => $key,
-                    'country_name' => $country['admin'] . ' (' . $key . ')',
-                ];
-            }
-            return ['data' => $countryArray];
+            return ['data' => $this->countries];
         } catch (Exception $exception) {
             Log::info($exception->getMessage());
             throw new Exception($exception->getMessage(), 422);
@@ -37,10 +33,62 @@ class CountryCodeService
     public function show($country)
     {
         try {
-            return Countries::where('cca3', $country)->first();
+            $countryData = collect($this->countries)->firstWhere('country_code', $country);
+            if (!$countryData) {
+                throw new Exception("Country not found", 404);
+            }
+            return $countryData;
         } catch (Exception $exception) {
             Log::info($exception->getMessage());
-            throw new Exception($exception->getMessage(), 422);
+            throw new Exception($exception->getMessage(), $exception->getCode() ?: 422);
         }
+    }
+
+    private function getCountriesList(): array
+    {
+        // The provided list of countries goes here
+        return [
+            [
+                "country_code" => "UGA",
+                "country_name" => "Uganda (UGA)"
+            ],
+            [
+                "country_code" => "KEN",
+                "country_name" => "Kenya (KEN)"
+            ],
+            [
+                "country_code" => "TZA",
+                "country_name" => "Tanzania (TZA)"
+            ],
+            [
+                "country_code" => "RWA",
+                "country_name" => "Rwanda (RWA)"
+            ],
+            [
+                "country_code" => "ZAF",
+                "country_name" => "South Africa (ZAF)"
+            ],
+            [
+                "country_code" => "NGA",
+                "country_name" => "Nigeria (NGA)"
+            ],
+            [
+                "country_code" => "GHA",
+                "country_name" => "Ghana (GHA)"
+            ],
+            [
+                "country_code" => "EGY",
+                "country_name" => "Egypt (EGY)"
+            ],
+            [
+                "country_code" => "DZA",
+                "country_name" => "Algeria (DZA)"
+            ],
+            [
+                "country_code" => "ETH",
+                "country_name" => "Ethiopia (ETH)"
+            ]
+        ]
+        ;
     }
 }
