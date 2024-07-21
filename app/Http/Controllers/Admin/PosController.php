dfa0749384a7d0adf227eb652e85ae6cecb2fa25
mike <omodingmike@gmail.com>
2024-07-21 12:53:12 +0300

    finished order updating.

--- app/Http/Controllers/Admin/PosController.php
+++ app/Http/Controllers/Admin/PosController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Enums\TaxType;
+use App\Http\Requests\CustomerRequest;
+use App\Http\Requests\PosOrderRequest;
+use App\Http\Resources\CustomerResource;
+use App\Http\Resources\OrderDetailsResource;
 use App\Models\Order;
 use App\Models\OrderItem;
-use Exception;
-use App\Services\OrderService;
 use App\Services\CustomerService;
-use App\Http\Requests\PosOrderRequest;
-use App\Http\Requests\CustomerRequest;
-use App\Http\Resources\OrderDetailsResource;
-use App\Http\Resources\CustomerResource;
+use App\Services\OrderService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class PosController extends AdminController
@@ -28,7 +29,7 @@ class PosController extends AdminController
         $this->middleware(['permission:pos'])->only('store');
     }
 
-    public function store(PosOrderRequest $request): \Illuminate\Http\Response | OrderDetailsResource | \Illuminate\Contracts\Foundation\Application | \Illuminate\Contracts\Routing\ResponseFactory
+    public function store(PosOrderRequest $request): \Illuminate\Http\Response|OrderDetailsResource|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         try {
             return new OrderDetailsResource($this->orderService->posOrderStore($request));
@@ -36,41 +37,88 @@ class PosController extends AdminController
             return response(['status' => false, 'message' => $exception->getMessage()], 422);
         }
     }
+
     public function update(Request $request)
     {
-        $order = Order::find($request->id);
-        $requestItems = Order::find($request->items);
-        $itemsArray   = [];
-        if (!blank($requestItems)) {
-            foreach ($requestItems as $item) {
-                $taxId          = isset($items[$item->item_id]) ? $items[$item->item_id] : 0;
-                $taxName        = isset($taxes[$taxId]) ? $taxes[$taxId]->name : null;
-                $taxRate        = isset($taxes[$taxId]) ? $taxes[$taxId]->tax_rate : 0;
-                $taxType        = isset($taxes[$taxId]) ? $taxes[$taxId]->type : TaxType::FIXED;
-                $taxPrice       = $taxType === TaxType::FIXED ? $taxRate : ($item->total_price * $taxRate) / 100;
-                $itemsArray[$i] = [
-                    'quantity'             => $item->quantity,
-                    'discount'             => (float)$item->discount,
-                    'price'                => $item->item_price,
-                    'item_variations'      => json_encode($item->item_variations),
-                    'item_extras'          => json_encode($item->item_extras),
-                    'instruction'          => $item->instruction,
-                    'item_variation_total' => $item->item_variation_total,
-                    'item_extra_total'     => $item->item_extra_total,
-                    'total_price'          => $item->total_price,
-                ];
-                $i++;
+        DB::transaction(function () use ($request) {
+            $order = Order::find($request->id);
+//        $requestItems = Order::find($request->items);
+            $requestItems = json_decode($request->items);
+            $order->update([
+                'subtotal' => $request->subtotal,
+                'total'    => $request->total,
+                'discount' => $request->discount,
+            ]);
+            $itemsArray = [];
+            if (!blank($requestItems)) {
+                $i = 0;
+                $only = [];
+                foreach ($requestItems as $item) {
+                    $taxId = isset($items[$item->item_id]) ? $items[$item->item_id] : 0;
+                    $taxName = isset($taxes[$taxId]) ? $taxes[$taxId]->name : null;
+                    $taxRate = isset($taxes[$taxId]) ? $taxes[$taxId]->tax_rate : 0;
+                    $taxType = isset($taxes[$taxId]) ? $taxes[$taxId]->type : TaxType::FIXED;
+                    $taxPrice = $taxType === TaxType::FIXED ? $taxRate : ($item->total_price * $taxRate) / 100;
+                    if (isset($item->id)) {
+                        $order_item = OrderItem::find($item->id);
+                        $only[] = $item->id;
+                        $order_item->update([
+                            'quantity'             => $item->quantity,
+                            'discount'             => (float)$item->discount,
+                            'price'                => $item->item_price,
+                            'item_variations'      => json_encode($item->item_variations),
+                            'item_extras'          => json_encode($item->item_extras),
+                            'instruction'          => $item->instruction,
+                            'item_variation_total' => $item->item_variation_total,
+                            'item_extra_total'     => $item->item_extra_total,
+                            'total_price'          => $item->total_price,
+                        ]);
+                    } else {
+                        $order_item =  OrderItem::create([
+                                'order_id'             => $order->id,
+                                'branch_id'             => $request->branch_id,
+                                'item_id'             => $item->item_id,
+                                'quantity'             => $item->quantity,
+                                'discount'             => (float)$item->discount,
+                                'price'                => $item->item_price,
+                                'item_variations'      => json_encode($item->item_variations),
+                                'item_extras'          => json_encode($item->item_extras),
+                                'instruction'          => $item->instruction,
+                                'item_variation_total' => $item->item_variation_total,
+                                'item_extra_total'     => $item->item_extra_total,
+                                'total_price'          => $item->total_price,
+                            ]
+                        );
+                        $only[] = $order_item->id;
+                    }
+//                    $itemsArray[$i] = [
+//                        'order_id'             => $order->id,
+//                        'quantity'             => $item->quantity,
+//                        'discount'             => (float)$item->discount,
+//                        'price'                => $item->item_price,
+//                        'item_variations'      => json_encode($item->item_variations),
+//                        'item_extras'          => json_encode($item->item_extras),
+//                        'instruction'          => $item->instruction,
+//                        'item_variation_total' => $item->item_variation_total,
+//                        'item_extra_total'     => $item->item_extra_total,
+//                        'total_price'          => $item->total_price,
+//                    ];
+                    $i++;
+                }
+                $order->orderItems()->whereNotIn('order_items.id', $only)->delete();
             }
-        }
-        if (!blank($itemsArray)) {
-            OrderItem::insert($itemsArray);
-        }
-        $this->order->save();
+//            if (!blank($itemsArray)) {
+//                OrderItem::where('order_id', $order->id)->delete();
+//                OrderItem::insert($itemsArray);
+//            }
+        });
+//        $order->save();
     }
 
     public function storeCustomer(
         CustomerRequest $request
-    ): \Illuminate\Http\Response|CustomerResource|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory {
+    ): \Illuminate\Http\Response|CustomerResource|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
         try {
             return new CustomerResource($this->customerService->store($request));
         } catch (Exception $exception) {
