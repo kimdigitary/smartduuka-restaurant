d5ead0504c3ff83ce7b70d29e36beae2a09206b1
barakhubs <markbrightbaraka22@gmail.com>
2024-09-13 20:05:27 +0300

    pulled changes before starting multitenancy

--- database/migrations/2024_06_29_120324_alter_branches_table.php
+++ database/migrations/2024_06_29_120324_alter_branches_table.php
@@ -8,9 +8,9 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('branches', function (Blueprint $table) {
-            DB::statement('ALTER TABLE branches MODIFY city VARCHAR(255) NULL');
-            DB::statement('ALTER TABLE branches MODIFY state VARCHAR(255) NULL');
-            DB::statement('ALTER TABLE branches MODIFY zip_code VARCHAR(255) NULL');
+            // DB::statement('ALTER TABLE branches MODIFY city VARCHAR(255) NULL');
+            // DB::statement('ALTER TABLE branches MODIFY state VARCHAR(255) NULL');
+            // DB::statement('ALTER TABLE branches MODIFY zip_code VARCHAR(255) NULL');
         });
     }
 
