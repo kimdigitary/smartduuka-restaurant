8e097bdb5bf5afe3ea043af370807cbe09544118
mike <omodingmike@gmail.com>
2024-10-03 14:09:56 +0300

    fixed pos order payment

--- app/Http/Resources/PosPaymentResource.php
+++ app/Http/Resources/PosPaymentResource.php
@@ -2,6 +2,9 @@
 
     namespace App\Http\Resources;
 
+    use App\Models\Order;
+    use App\Models\PaymentMethod;
+    use App\Models\PosPayment;
     use Illuminate\Http\Request;
     use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -9,8 +12,11 @@
     {
         public function toArray(Request $request) : array
         {
+            $purchasePaymentAmount = floatval(PosPayment::where('order_id', $this->id)->sum('amount'));
+            $due_payment = (float) $this->total - $purchasePaymentAmount;
             return [
-                'due_payment' => ( floatval($this->total) - floatval($this?->purchasePayment?->sum('amount')) ) ,
+                'due_payment'     => $due_payment ,
+                'payment_methods' => PaymentMethod::all() ,
             ];
         }
     }
